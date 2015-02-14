4b2af5c3419ec48b9bc8ae092a5b99b9e4e40509
samhamm <samhamm@gmail.com>
2015-02-14 14:25:33 -0800

    basic server functionality; still fleshing out all routes

--- scratch/gaye-1030.js
+++
@@ -1,7 +0,0 @@
-else if (req.method === 'DELETE') {
-           fs.unlink('./data/' + filename, function(err) {
-               res.writeHead(err ? 404 : 200);
-               res.end();
-           });
-
- }
