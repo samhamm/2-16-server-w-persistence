9f12daef320ba7bc2c05a24d092564d877f6a31e
samhamm <samhamm@gmail.com>
2015-02-14 16:58:08 -0800

    after lots of refactoring... it's alive

--- server.js
+++ server.js
@@ -5,25 +5,32 @@
 'use strict';
 
 var http = require('http');
-var route = require('./lib/router');
+var router = require('./lib/router');
 
 var routes = {};
-routes['data'] = route;
+routes['daleks'] = router;
 
-var server = http.createServer(function(req, res){
-    var pathBits = req.url.split('/');
-    if(typeof(routes[pathBits[1]]) === 'function') {
-        routes[pathBits[1]](req, res);
-    } else {
-        res.writeHead(404, {
-            'Content-Type': 'application/json'
-        });
+var server = http.createServer(function(req, res) {
+  var pathBits = req.url.split("/");
 
-        res.write(JSON.stringify({msg: '404: Page not found!'}));
-        res.end();
-    }
+  if (typeof(routes[pathBits[1]]) === 'function') {
+    routes[pathBits[1]](req, res);
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'application/json'
+    });
+    console.log(pathBits);
+    console.log(pathBits[0]);
+    res.write(JSON.stringify({msg: 'page not found'}));
+    res.end();
+  }
 });
 
 server.listen(3000, function() {
-    console.log('The server is listening at localhost:3000');
+  console.log('server listening');
 });
+
+/* Once server is going, run this in another Terminal tab
+$ superagent localhost:3000/unicorns post {"hello": "world", "foo": "bar"} */
+
+// https://github.com/toastynerd/superagent-cli
